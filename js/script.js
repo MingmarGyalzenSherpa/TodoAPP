da1a8045531501ce9c5b3ddd42c40601d8f8de7b
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2023-04-07 20:50:05 +0545

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const taskInput = document.querySelector(".taskInput");
+const submitBtn = document.querySelector(".submit");
+const TASKLIST = document.querySelector(".tasks");
+console.log(TASKLIST);
+console.log(TASK_INPUT);
+
+TASK_INPUT.addEventListener("submit", (e) => {
+  console.log("Entered");
+});
