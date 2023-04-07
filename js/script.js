5fd78ed2e97c972b67f342b12c74224e4ab6035d
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2023-04-07 21:47:31 +0545

    added insert feature

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
-const taskInput = document.querySelector(".taskInput");
+const taskInputEl = document.querySelector(".taskInput");
 const submitBtn = document.querySelector(".submit");
-const TASKLIST = document.querySelector(".tasks");
-console.log(TASKLIST);
-console.log(TASK_INPUT);
+const taskList = document.querySelector(".tasks");
 
-TASK_INPUT.addEventListener("submit", (e) => {
-  console.log("Entered");
+let todos = JSON.parse(localStorage.getItem("tasks"));
+let items;
+
+todos.forEach((todo) => {
+  appendToList(todo);
 });
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  todos.push(taskInputEl.value);
+  appendToList(taskInputEl.value);
+  taskInputEl.value = "";
+
+  localStorage.setItem("tasks", JSON.stringify(todos));
+});
+
+function appendToList(task) {
+  let listItem = document.createElement("li");
+  listItem.textContent = task;
+  taskList.insertAdjacentElement("afterbegin", listItem);
+}
