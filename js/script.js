7cd12eed6e37ffebeeb6c98e3ca3f507edc2f13a
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2023-04-08 16:01:14 +0545

    added bg and fixed some ui

--- js/script.js
+++ js/script.js
@@ -2,24 +2,29 @@ const taskInputEl = document.querySelector(".taskInput");
 const submitBtn = document.querySelector(".submit");
 const taskList = document.querySelector(".tasks");
 
-let todos = JSON.parse(localStorage.getItem("tasks"));
+let todos = JSON.parse(localStorage.getItem("tasks")) || [];
 let items;
-
-todos.forEach((todo) => {
+let count = 0;
+console.log(count);
+todos?.forEach((todo) => {
   appendToList(todo);
 });
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  todos.push(taskInputEl.value);
-  appendToList(taskInputEl.value);
-  taskInputEl.value = "";
-
+  let task = [];
+  if (taskInputEl.value != "") {
+    todos.push(taskInputEl.value);
+    appendToList(taskInputEl.value);
+    taskInputEl.value = "";
+    console.log(todos);
+  }
   localStorage.setItem("tasks", JSON.stringify(todos));
 });
 
 function appendToList(task) {
   let listItem = document.createElement("li");
+  listItem.dataset.id = ++count;
   listItem.textContent = task;
   taskList.insertAdjacentElement("afterbegin", listItem);
 }
